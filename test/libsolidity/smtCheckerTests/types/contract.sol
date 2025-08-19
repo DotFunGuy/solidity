20f25e53d14e36af0a70aadd8ed3846f84f6740a
Matheus Aguiar <matheus.pit@gmail.com>
2025-08-18 21:18:19 -0300

    Update contract type deprecation warning tests

--- test/libsolidity/smtCheckerTests/types/contract.sol
+++ test/libsolidity/smtCheckerTests/types/contract.sol
@@ -8,4 +8,5 @@ contract C
 // SMTEngine: all
 // SMTIgnoreCex: yes
 // ----
-// Warning 6328: (51-65): CHC: Assertion violation happens here.
+// Warning 9170: (58-64): Comparison of variables of contract type is deprecated and scheduled for removal in the next breaking version (0.9). Instead, use an explicit cast to address type.
+// Warning 6328: (51-65): CHC: Assertion violation happens here.\nCounterexample:\n\nc = 0\nd = 1\n\nTransaction trace:\nC.constructor()\nC.f(0, 1)
--- test/libsolidity/smtCheckerTests/types/contract_2.sol
+++ test/libsolidity/smtCheckerTests/types/contract_2.sol
@@ -13,4 +13,5 @@ contract C
 // SMTEngine: all
 // SMTIgnoreCex: yes
 // ----
-// Warning 6328: (76-90): CHC: Assertion violation happens here.
+// Warning 9170: (83-89): Comparison of variables of contract type is deprecated and scheduled for removal in the next breaking version (0.9). Instead, use an explicit cast to address type.
+// Warning 6328: (76-90): CHC: Assertion violation happens here.\nCounterexample:\n\nc = 0\nd = 1\n\nTransaction trace:\nC.constructor()\nC.f(0, 1)

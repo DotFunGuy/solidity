11ac95726f2317010589cf7acf5f95966b04939c
Matheus Aguiar <matheus.pit@gmail.com>
2025-08-19 01:14:36 -0300

    Update send/transfer warning tests

--- test/libsolidity/syntaxTests/nameAndTypeResolution/405_address_checksum_type_deduction.sol
+++ test/libsolidity/syntaxTests/nameAndTypeResolution/405_address_checksum_type_deduction.sol
@@ -4,3 +4,4 @@ contract C {
     }
 }
 // ----
+// Warning 9207: (47-107): 'transfer' is deprecated and scheduled for removal in the next breaking version (0.9). It is encouraged to use 'call{value: <amount>}()' instead.
